f7de25e6dfcf4913c251f7d950f6116bfd03e6e7
Yoann BETTINELLI <yoann1805@gmail.com>
2021-04-21 10:58:10 +0200

    gestion des urls urlGeneratorInterface

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
-    /**
+    /** Permet d'afficher des produits en fonction d'une catégory
      * @Route("/{slug}", name="product_category")
+     * @param $slug
+     * @param CategoryRepository $categoryRepository
+     * @return Response
      */
     public function category($slug, CategoryRepository $categoryRepository): Response
     {
@@ -26,6 +31,29 @@ class ProductController extends AbstractController
         return $this->render('product/category.html.twig', [
             'slug' => $slug,
             'category' => $category
+        ]);
+    }
+
+
+    /**
+     * Permet d'afficher la page d'un produit
+     * @Route("/{category_slug}/{slug}", name="product_show")
+     * @param $slug
+     * @param ProductRepository $productRepository
+     * @return Response
+     */
+    public function show($slug, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
